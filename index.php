0ad75146da81b22a076354d6deeb3fb75af1b0db
Piero Cusi Ruiz <pcusir@gmail.com>
2021-09-25 02:52:30 -0500

    Added first section

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/scss/styles.css">
+    <title>Nexo Cash</title>
+</head>
+
+<body>
+    <?php require_once('./shared/header.php'); ?>
+    <div class="container">
+        <div class="container__investment">
+            <div class="container__investment__info">
+                <h1 class="title">
+                    Invierte y presta dinero de forma
+                </h1>
+                <div class="check">
+                    <ul class="check__ul">
+                        <li class="item">
+                            <a class="shape">Fácil</a>
+                        </li>
+                        <li class="item">
+                            <a class="shape">Rápida</a>
+                        </li>
+                        <li class="item">
+                            <a class="shape">Segura</a>
+                        </li>
+                    </ul>
+                </div>
+                <pre class="description">
+                    La mejor alternativa a los préstamos tradicionales. 
+                    Aplica sin muchos requisitos, 
+                    sin tanto papeleo y de forma Online de 
+                    la mano de asesores especializados.
+                </pre>
+            </div>
+            <div class="container__investment__img">
+                <img src="./public/assets/img/nexo_cash_sales.png" class="sales__img">
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
