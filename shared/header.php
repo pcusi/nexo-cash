a914d60fea911318a11f4f5b862a98f761e721ba
Piero Cusi Ruiz <pcusir@gmail.com>
2021-09-25 03:53:57 -0500

    Added four section

--- shared/header.php
+++ shared/header.php
@@ -1,14 +1,16 @@
 <header class="header">
-    <p>hola mundo</p>
+    <div class="header__brand">
+        <img src="./public/assets/img/nexo_cash_logo.png" class="img">
+    </div>
     <ul class="header__ul">
         <li class="item">
-            <a href="" class="link">Préstamo</a>
+            <a class="link">Préstamo</a>
         </li>
         <li class="item">
-            <a href="" class="link">Invierte</a>
+            <a class="link">Invierte</a>
         </li>
         <li class="item">
-            <a href="" class="link">Conoce más</a>
+            <a class="link">Conoce más</a>
         </li>
     </ul>
 </header>
